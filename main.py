68971ac2dbcb1bc19ae3d694b18277b2258dc65a
abymar1587 <abymar1587@gmail.com>
2022-11-09 22:25:15 +0100

    Se agrega nueva ruta utilizando path parameter

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ app = FastAPI()
 #path-operations-functions
 #Metodos get
 
-#Ruta root
+#Ruta root-ruta principal
 @app.get("/") #path:"/", operation:get
 async def root(): #function: root() [corresponde a la funcion que se encuentra debajo del decorador]
     return {"message": "Hello World"}
+
+#Path parameters, parametros o variables que se pasan atraves de la ruta
+@app.get("/items/{item_id}")#path:"/items/{item_id(este es el parametro)}", operation:get
+async def read_item(item_id: int):#El parametro item_id pasa a la funcion como argumento, con ": int(tipo de dato)" la funcion limita el tipo de dato
+    return {"item_id": item_id}
