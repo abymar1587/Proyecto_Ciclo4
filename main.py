437ccfbb2a23b89a841f077ca9897ef169ac331a
abymar1587 <abymar1587@gmail.com>
2022-11-09 23:35:15 +0100

    Se agrega a la misma ruta requet body, utilizando metodo post

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel #para declarar un request body se debe importar BaeModel de pydantic
+from typing import Union
+
+
+class Item(BaseModel): ##se mandan datos de un cliente a la API, se utilizara la operacion post(ingresar nueva informacion)
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
 
 #instancia
 app = FastAPI()
@@ -22,3 +31,8 @@ fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"
 @app.get("/items/")#Se puede pasar en la ruta de manera opcional(de eta manera: ?skip=0&limit=10)para una consulta n especifico, de lo contrario mostrara lo definido en la funcion
 async def read_item(skip: int = 0, limit: int = 10):
     return fake_items_db[skip : skip + limit]
+
+#Metodo Post
+@app.post("/items/")#path:"/items/", operation:post
+async def create_item(item: Item):#Item e el modelo, agregar item a la base de datos
+    return item
